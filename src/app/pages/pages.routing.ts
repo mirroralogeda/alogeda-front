b8126200b4357749d1b25f958140968535e73a56
Dionatan Zimmermann <zimmermanndionatan@gmail.com>
2018-05-26 14:49:09 -0300

    Adicionado menu de Cidades

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -28,6 +28,7 @@ export const routes: Routes = [
       { path: 'forms', loadChildren: './forms/forms.module#FormsModule' },
       { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
       { path: 'maps', loadChildren: './maps/maps.module#MapsModule' },
+      { path: 'cadastros/cidades', loadChildren: './cidades/cidades.module#CidadesModule' },
       { path: 'cadastros/setores', loadChildren: './setores/setores.module#SetoresModule' },
       { path: 'cadastros/cargos', loadChildren: './cargos/cargos.module#CargosModule' },
       { path: 'cadastros/tab-irrf', loadChildren: './tab-irrf/tab-irrf.module#Tab_irrfModule' }
