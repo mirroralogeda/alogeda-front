9319326ee9a91c6c53a28700b65dde4bc5e6fd74
Elenara <elenara.hein@gmail.com>
2018-06-02 10:24:05 -0300

    Cadastro de dependentes

--- src/app/pages/dependentes/dependentes.component.ts
+++ src/app/pages/dependentes/dependentes.component.ts
@@ -1,21 +1,116 @@
 import { Component, OnInit } from '@angular/core';
 import { Resposta } from "./resposta.model";
 import { DependentesService } from "./dependentes.service";
-
+import { LocalDataSource } from 'ng2-smart-table';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'dependentes',
   templateUrl: "./dependentes.component.html",
   providers: [DependentesService]
 })
 export class DependentesComponent implements OnInit {
-  
-  teste = "Ola";
-  resposta: Resposta;
 
-  constructor( private DependentesService: DependentesService ) {}
-  
-  ngOnInit(){
-    this.DependentesService.getAllDependentes()
-      .subscribe( (dados) => this.resposta = dados );
+  teste = "Ola";
+  resposta: Resposta = {
+    status: "carregando",
+    result: []
+  };
+
+  constructor(private DependentesService: DependentesService, private route: ActivatedRoute) { }
+
+  settingsTable = {
+    add: {
+      addButtonContent: '<i class="ion-ios-plus-outline"></i>',
+      createButtonContent: '<i class="ion-checkmark"></i>',
+      cancelButtonContent: '<i class="ion-close"></i>',
+      confirmCreate: true,
+    },
+    edit: {
+      editButtonContent: '<i class="ion-edit"></i>',
+      saveButtonContent: '<i class="ion-checkmark"></i>',
+      cancelButtonContent: '<i class="ion-close"></i>',
+      confirmSave: true
+    },
+    delete: {
+      deleteButtonContent: '<i class="ion-trash-a"></i>',
+      confirmDelete: true
+    },
+    columns: {
+      dataNascimento: {
+        title: 'Data Nascimento',
+        type: 'date'
+      },
+      cpf: {
+        title: 'CPF',
+        type: 'number'
+      },
+      certidaoNascimento: {
+        title: 'Certidão de Nascimento',
+        type: 'string'
+      },
+      impostoRenda: {
+        title: 'Imposto de renda',
+        type: 'string'
+      },
+      auxilioCreche: {
+        title: 'Auxilio Creche',
+        type: 'string'
+      },
+      salarioFamilia: {
+        title: 'Salario Família',
+        type: 'string'
+      },
+      nome: {
+        title: 'Nome do Funcionario.',
+        type: 'string'
+      },
+    }
+  };
+  sourceTable: LocalDataSource = new LocalDataSource();
+
+  private sub: any;
+  private id: number;
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      this.id = +params['id']; // (+) converts string 'id' to a number
+      this.DependentesService.getAllDependentes(this.id, (dados) => {
+        this.sourceTable.load(dados.data.result)
+      })
+      
+    });
+  }
+  addRecord(event) {
+    event.newData.funcionarios = { id: this.id }
+    this.DependentesService.add(event.newData, () => {
+      event.confirm.resolve(event.newData);
+      this.DependentesService.getAllDependentes(this.id, (dados) => {
+        this.sourceTable.load(dados.data.result)
+      })
+    })
+  }
+  updateRecord(event) {
+    event.newData.funcionarios = { id: this.id }
+    
+    this.DependentesService.add(event.newData, () => {
+      event.confirm.resolve(event.newData);
+      this.DependentesService.getAllDependentes(this.id, (dados) => {
+        this.sourceTable.load(dados.data.result)
+      })
+    })
+  }
+  onDeleteConfirm(event): void {
+    if (window.confirm('Quer mesmo excluir o registro?')) {
+      this.DependentesService.delete(event.data, () => {
+        event.confirm.resolve();
+        this.DependentesService.getAllDependentes(this.id, (dados) => {
+          this.sourceTable.load(dados.data.result)
+        })
+      })
+    } else {
+      event.confirm.reject();
+    }
+  }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
--- src/app/pages/dependentes/dependentes.module.ts
+++ src/app/pages/dependentes/dependentes.module.ts
@@ -5,11 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { DependentesComponent } from './dependentes.component';
 import { routing } from './dependentes.routing';
 
+
+import { NgaModule } from '../../theme/nga.module';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { HttpModule } from '@angular/http';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    routing
+    routing,
+    
+    Ng2SmartTableModule,
+    routing,
+    HttpModule,
+    NgaModule
   ],
   declarations: [
     DependentesComponent
